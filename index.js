8cf1c041f15a7f72133d866be0af051fd3d4c558
Atomos Technologies <atomostechnologies@Atomoss-Air.bbrouter>
2022-09-14 16:59:46 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://192.168.1.50:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("send_msg", (data) => {
+    socket.broadcast.emit("receive_msg", data);
+    console.log(data);
+  });
+
+  //   console.log(`User Connected:${socket.id}`);
+  //   console.log(socket);
+});
+
+server.listen(3001, () => {
+  console.log("Server Started on port 3001");
+});
