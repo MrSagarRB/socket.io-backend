efc3be213e2552bd68ddf5f343505a3595fc719e
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2023-01-18 11:31:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,29 +4,27 @@ var http = require("https");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-app.use(cors());
-const server = http.createServer(app);
+// app.use(cors());
+// const server = http.createServer(app);
 
 app.get("/", (req, res) => {
   res.send("Server Is Started on Port 3001");
 });
 
-const io = new Server(server, {
-  cors: {
-    origin: "https://socket-io-front-end.vercel.app",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "https://socket-io-front-end.vercel.app",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  socket.on("send_msg", (data) => {
-    socket.broadcast.emit("receive_msg", data);
-    console.log(data);
-  });
+// io.on("connection", (socket) => {
+//   socket.on("send_msg", (data) => {
+//     socket.broadcast.emit("receive_msg", data);
+//     console.log(data);
+//   });
 
-  //   console.log(`User Connected:${socket.id}`);
-  //   console.log(socket);
-});
+// });
 
 server.listen(3001, () => {
   console.log("Server Started on port 3001");
